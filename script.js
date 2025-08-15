5149713f26e9a68f4c194767faafcd67886373d9
Aleksandra Mate <matealeksandra@gmail.com>
2025-08-15 11:23:04 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 const sortButton = document.getElementById("sort");
 
 const sortInputArray = (event) => {
+  event.preventDefault();
+};const sortButton = document.getElementById("sort");
+
+const sortInputArray = (event) => {
 
 };
+

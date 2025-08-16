3670ae27200195cb3217460179bd549a824f695c
Aleksandra Mate <matealeksandra@gmail.com>
2025-08-16 11:15:01 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,20 @@ const sortButton = document.getElementById("sort");
 
 const sortInputArray = (event) => {
   event.preventDefault();
+
+  const inputValues = [...document.getElementsByClassName("values-dropdown")]
+    .map(dropdown => dropdown.value);
+
+  console.log(inputValues);
+};
+
+sortButton.addEventListener("click", sortInputArray);const sortButton = document.getElementById("sort");
+
+const sortInputArray = (event) => {
+  event.preventDefault();
   const inputValues = [...document.getElementsByClassName("values-dropdown")].map((dropdown) => dropdown.value);
   
 }
 
 sortButton.addEventListener("click", sortInputArray);
+

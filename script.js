139b5d3b06d0191b92ab5a0cf57a1bd331e98199
Aleksandra Mate <matealeksandra@gmail.com>
2025-08-16 11:25:54 +0300

    Update script.js

--- script.js
+++ script.js
@@ -6,13 +6,14 @@ const sortInputArray = (event) => {
   const inputValues = [
     ...document.getElementsByClassName("values-dropdown")
   ].map((dropdown) => Number(dropdown.value));
+  updateUI(inputValues);
 }
 
 const updateUI = (array = []) => {
   array.forEach((num, i) => {
     const outputValueNode = document.getElementById(`output-value-${i}`);
     outputValueNode.innerText = num;
-  });
-};
+  })
+}
 
 sortButton.addEventListener("click", sortInputArray);

6bda3066f3549b8013e189cb02921023abdf0e19
Aleksandra Mate <matealeksandra@gmail.com>
2025-08-17 12:39:21 +0300

    Update script.js

--- script.js
+++ script.js
@@ -24,13 +24,15 @@ const bubbleSort = (array) => {
     for (let j = 0; j < array.length - 1; j++) {
       console.log(array, array[j], array[j + 1]);
 
-if (array[j] > array[j + 1]) {
-  const temp = array[j];
-  array[j] = array[j + 1];
-  array[j + 1] = temp;
-}
+      if (array[j] > array[j + 1]) {
+        const temp = array[j];
+        array[j] = array[j + 1];
+        array[j + 1] = temp;
+      }
     }
   }
+
+  return array;
 }
 
 sortButton.addEventListener("click", sortInputArray);

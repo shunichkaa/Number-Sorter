485af58ce14ce448420ec6c6c77bd723ffd49b6f
Aleksandra Mate <matealeksandra@gmail.com>
2025-08-18 13:02:10 +0300

    Update script.js

--- script.js
+++ script.js
@@ -35,9 +35,13 @@ const bubbleSort = (array) => {
 
 const selectionSort = (array) => {
   for (let i = 0; i < array.length; i++) {
-    let minIndex = i
-    for (let j = i + 1; j < array.length; j++) {
+    let minIndex = i;
 
+    for (let j = i + 1; j < array.length; j++) {
+      console.log(array, array[j], array[minIndex])
+      if (array[j] < array[minIndex]) {
+        minIndex = j
+      }
     }
   }
 }

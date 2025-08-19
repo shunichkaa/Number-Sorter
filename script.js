bbb4a5cc31c09b25d3152b4d95e340eac05655e6
Aleksandra Mate <matealeksandra@gmail.com>
2025-08-19 17:37:40 +0300

    Update script.js

--- script.js
+++ script.js
@@ -53,7 +53,8 @@ const selectionSort = (array) => {
 
 const insertionSort = (array) => {
   for (let i = 1; i < array.length; i++) {
-    
+    const currValue = array[i]
+    let j = i - 1
   }
 }
 
